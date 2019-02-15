56d88a76318b4767f872fe073c47fc495a7e6587
Inaki Villar <inaki.seri@gmail.com>
2019-02-15 18:28:58 +0700

    updating to 0.1.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.11")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
         classpath("com.android.tools.build:gradle:3.3.0")
-        classpath("com.cdsap:talaiot:0.1.10")
+        classpath("com.cdsap:talaiot:0.1.11")
     }
 }
 
