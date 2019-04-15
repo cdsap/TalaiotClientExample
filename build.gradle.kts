63b6e7a572da19b98db61273525b250f246af45b
Inaki Villar <inaki.seri@gmail.com>
2019-04-15 08:00:06 +0530

    adding version 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
-//import org.jetbrains.kotlin.ir.backend.js.compile
-import java.net.URI
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-
 buildscript {
     repositories {
         mavenCentral()
@@ -15,8 +9,8 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.11")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
-        classpath("com.android.tools.build:gradle:3.3.0")
-        classpath("com.cdsap:talaiot:0.1.16")
+        classpath("com.android.tools.build:gradle:3.3.1")
+        classpath("com.cdsap:talaiot:0.1.17")
     }
 }
 
