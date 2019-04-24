8d1bfcf82ce29b9d4ee16443a46d478174443cef
Inaki Villar <inaki.seri@gmail.com>
2019-04-24 20:19:48 +0700

    updating to 0.1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@ buildscript {
 
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.11")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
-        classpath("com.android.tools.build:gradle:3.3.1")
-        classpath("com.cdsap:talaiot:0.1.17")
+        classpath("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+        classpath("com.android.tools.build:gradle:3.3.2")
+        classpath("com.cdsap:talaiot:0.1.18")
     }
 }
 
