3c1ab553fcf5543b2c0ec5e1512de33a5552365d
Inaki Villar <inaki.seri@gmail.com>
2019-05-09 21:45:36 -0700

    updating to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.21")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
         classpath("com.android.tools.build:gradle:3.3.2")
-        classpath("com.cdsap:talaiot:0.1.18")
+        classpath("com.cdsap:talaiot:0.2.0git st")
     }
 }
 
